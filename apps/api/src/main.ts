468eccfe5297643bff0e980115ac638eedde437e
Petar Cvetko Voćanec <cvetkovocanecpetar@gmail.com>
2022-06-22 19:28:08 +0200

    Added Swagger to the API

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,6 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -13,6 +9,20 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  const config = new DocumentBuilder()
+    .setTitle('Money Manager API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document, {
+    swaggerOptions: {
+      tryItOutEnabled: true,
+    },
+  });
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
