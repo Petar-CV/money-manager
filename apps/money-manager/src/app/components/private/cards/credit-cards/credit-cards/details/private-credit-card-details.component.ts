ba4124a11ef5381e9868b79504dce5ab5b3acd5f
Petar Cvetko Voćanec <cvetkovocanecpetar@gmail.com>
2022-07-31 20:26:47 +0200

    Removed unused code

--- apps/money-manager/src/app/components/private/cards/credit-cards/credit-cards/details/private-credit-card-details.component.ts
+++ apps/money-manager/src/app/components/private/cards/credit-cards/credit-cards/details/private-credit-card-details.component.ts
@@ -1,79 +1,8 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  OnInit,
-} from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-
-import { ICreditCardIssuer } from '@petar-cv/money-manager-models';
-import { CreditCardsService } from 'apps/money-manager/src/app/shared/services/entities/private/credit-cards/credit-cards.service';
-import { CreditCardIssuersService } from 'apps/money-manager/src/app/shared/services/entities/private/credit-card-issuers/credit-card-issuers.service';
-import { PrivateCreditCardsRoutes } from 'apps/money-manager/src/app/shared/constants/routing';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'petar-cv-private-credit-card-details',
   templateUrl: './private-credit-card-details.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PrivateCreditCardDetailsComponent implements OnInit {
-  public creditCardIssuers$: Observable<Partial<ICreditCardIssuer>[]>;
-  private currentId?: string;
-
-  creditCardForm = this.formBuilder.nonNullable.group({
-    name: ['', Validators.required],
-    limit: [0, Validators.required],
-    billingDate: [1, Validators.required],
-    issuerId: ['', Validators.required],
-  });
-
-  constructor(
-    private readonly formBuilder: FormBuilder,
-    private readonly route: ActivatedRoute,
-    private readonly router: Router,
-    private readonly cdr: ChangeDetectorRef,
-    private readonly creditCardsService: CreditCardsService,
-    private readonly creditCardIssuersService: CreditCardIssuersService
-  ) {
-    this.creditCardIssuers$ = this.creditCardIssuersService.findAllLov();
-  }
-
-  public ngOnInit(): void {
-    const currentId = this.route.snapshot.paramMap.get('id');
-
-    if (currentId) {
-      this.currentId = currentId;
-      this.fetchEntityDetailsAndLoadIntoForm();
-    }
-  }
-
-  private fetchEntityDetailsAndLoadIntoForm(): void {
-    if (this.currentId) {
-      this.creditCardsService
-        .findOne(this.currentId)
-        .subscribe((creditCard) => {
-          if (creditCard) {
-            this.creditCardForm.patchValue({
-              ...creditCard,
-            });
-
-            this.cdr.markForCheck();
-          } else {
-            this.router.navigate([
-              PrivateCreditCardsRoutes.PRIVATE_CREDIT_CARDS,
-            ]);
-          }
-        });
-    }
-  }
-
-  public onFormSubmit(): void {
-    const entityData = this.creditCardForm.value;
-
-    if (this.currentId) {
-      this.creditCardsService.update(entityData, this.currentId).subscribe();
-    }
-  }
-}
+export class PrivateCreditCardDetailsComponent {}
