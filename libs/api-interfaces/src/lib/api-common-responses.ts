8268efd6c98d94cf5d6ff369f14c2db0a696dee0
Petar Cvetko Voćanec <cvetkovocanecpetar@gmail.com>
2022-10-02 01:06:21 -0700

    Added missing errors to the translations

--- libs/api-interfaces/src/lib/api-common-responses.ts
+++ libs/api-interfaces/src/lib/api-common-responses.ts
@@ -3,4 +3,5 @@ export enum CommonResponses {
   NOT_IMPLEMENTED = 'global.responses.not-implemented',
   IMAGE_DELETED = 'global.responses.image-deleted',
   IMAGE_NOT_FOUND = 'global.responses.image-not-found',
+  NOT_FOUND = 'global.responses.not-found',
 }
